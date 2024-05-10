a5ba58b2c0a30ca8215126ffe22a452c012ae50d
maxrisher <max.risher@gmail.com>
2024-05-09 17:08:54 -0700

    adding authentication service

--- lib/services/authentication_service.dart
+++ lib/services/authentication_service.dart
@@ -1,7 +1,11 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:flutter/material.dart';
 
 class AuthenticationService {
+  final _storage = FlutterSecureStorage();
+
   Future<bool> login(String email, String password) async {
     try {
       var response = await http.post(
@@ -16,6 +20,9 @@ class AuthenticationService {
       );
       if (response.statusCode == 200) {
         // Store the token and other necessary data
+        var django_auth_token = jsonDecode(response.body)['token'];
+        await _storage.write(key: 'app_token', value: django_auth_token);
+        notifyListeners();
         return true;
       }
       return false;
@@ -25,8 +32,30 @@ class AuthenticationService {
     }
   }
 
-  Future<void> signUp(String email, String password) async {
-    // Your sign up logic using your preferred authentication method (e.g., Firebase)
+  Future<bool> signUp(String email, String password) async {
+    try{
+      var response = await http.post(Uri.parse(uri),
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(<String, String>{
+        'email': email,
+        'password': password,
+      }),
+      );
+      if (response.statusCode == 200){
+        return true;
+      }
+      return false;
+    } catch (e) {
+      print('Error signing up: $e');
+      return false;
+    }
+  }
+  Future<void> logOut() async{
+    _storage.delete(key: 'django_auth_token');
+    notifyListeners();
+    print("logged out");
   }
 }
 
